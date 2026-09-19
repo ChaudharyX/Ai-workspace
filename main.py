542c742310f0df7f3d694bba327a57ca93321d20
ChaudharyX <amanc5229@gmail.com>
2026-09-19 10:41:53 +0530

    Fix return statement in FileResponse handling

--- main.py
+++ main.py
@@ -863,7 +863,7 @@ def download(filename):
             "File not found"
         )
 
-    return FileResponse(index_file, media_type="text/html")
+    return FileResponse
         path,
         filename=filename,
         media_type=(
